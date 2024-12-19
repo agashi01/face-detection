42ca68770e36dc87d031c7aafe3acf67a7788051
Andi Gashi <andigashi2005@gmail.com>
2024-12-19 21:33:30 +0100

    only database needed

--- src/Logo.js
+++ src/Logo.js
@@ -1,22 +1,21 @@
 import React, { useRef } from 'react';
 import Tilt from 'react-tilt';
-import brain from './brain.png';
+import brain from './brain.webp';
 import './Logo.css';
 
 const Logo = () => {
 	const tiltRef = useRef(null); // Create a ref for the Tilt component
 
 	return (
-		<div className='ma4 mt0'>
+		<div style={{display: "flex",justifyContent: "center"}} className='ma4 mt0'>
 			<Tilt
 				ref={tiltRef} // Attach the ref here
 				className='Tilt br2 shadow-2'
 				options={{ max: 55 }}
-				style={{ height: 200, width: 200 }}
+				style={{ display:"flex",justifyContent:"center",alignItems:"center" ,height: 200, width: 200 }}
 			>
 				<div className="Tilt-inner pa3">
 					<img
-						style={{ paddingTop: '30px', paddingLeft: '30px' }}
 						alt='logo'
 						src={brain}
 					/>
